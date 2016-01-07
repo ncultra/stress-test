950b525caf1b48480aa4c4d43bd0dcb143eddce7
Mike Day <ncmike@ncultra.org>
2016-01-07 11:50:35 -0500

    meta-script

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+wait
 cd linux.git
+echo "building linux kernel"
 make defconfig
+wait
 make
 rm -fvr linux.git
-
+cd ..
