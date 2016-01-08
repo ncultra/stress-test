a88dd05bb43de31429b9d0d8e9b1b4da771760aa
Mike Day <ncmike@ncultra.org>
2016-01-08 15:52:05 -0500

    genericize the script

--- run-dd-test.sh
+++ run-dd-test.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/bash
 
-CHILDREN=100
-
+CHILDREN=$1
+shift
 while (( $CHILDREN > 0))
 do
-    dd if=/dev/random | bzip2 -1 >/dev/null & 
-    
+#    dd if=/dev/random | bzip2 -1 >/dev/null & 
+    $($* &>/dev/null ) & 
     (( CHILDREN -= 1 ))
 done
-
 echo "waiting"
 wait
-killall dd
-
+#killall dd
+killall $1
