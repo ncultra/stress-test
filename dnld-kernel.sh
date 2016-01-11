818a5a96297cfaa96ffebc33f76631c9dc8606f3
Mike Day <ncmike@ncultra.org>
2016-01-11 15:58:33 -0500

    do not write kernel.tar.gz to disk

--- dnld-kernel.sh
+++ dnld-kernel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --no-check-certificate https://cdn.kernel.org/pub/linux/kernel/v4.x/testing/linux-4.4-rc8.tar.xz
+wget --no-check-certificate https://cdn.kernel.org/pub/linux/kernel/v4.x/testing/linux-4.4-rc8.tar.xz >/dev/null
