8438f8a8f006389959b62ee454ab38a12c84809b
Mike Day <ncmike@ncultra.org>
2016-01-13 13:33:24 -0500

    new functionality in the domains script

--- domains.sh
+++ domains.sh
@@ -1,16 +1,54 @@
 #!/usr/bin/env bash
 
 
+# $1 == how many seconds to sleep before destroying domains
+# $2 == number of domains to start/stop
+
+
+# bash generate random 32 character alphanumeric string (lowercase only)
+
+
 if (( $EUID )) ; then
 echo "you need root privileges"
     exit 1
 fi
 
-echo "trashing mini-os domain"
+
+
+#TODO pass the config in as a option
+create_minios() {
+
+    domfile=$(mktemp "domXXXX") 
+    cat<<EOF>$domfile
+kernel = "mini-os.gz"
+memory = "32"
+name = "$1"
+EOF
+
+    xl create $domfile
+    cat $domfile
+    rm $domfile
+}
+
+declare -a domains
+INDEX=$2
 
 while [ 1 ] ; do
 
-    xl create minios/domain_config
+# create an array of $INDEX random elements
+until (( "${#domains[@]}" == "$INDEX" )); do
+    domains=( "${domains[@]}"\
+      "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)")
+done
+
+echo "${#domains[@]}"
+    for el in ${domains[@]} ; do
+	create_minios $el
+    done
     sleep $1
-    echo xl destroy mos
+    
+    for el in ${domains[@]} ; do
+	xl destroy $el
+    done
+   
 done
