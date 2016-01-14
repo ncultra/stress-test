cf5d027373e1575c7def4c3c8e9d47c7e283a10f
Mike Day <ncmike@ncultra.org>
2016-01-14 11:46:05 -0500

    added a file that will eventually manage children

---
+++ children.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# this file intended to be sourced by an executable script
+
+# $1 is the pid to store
+record_child_pid() {
+    f=$(mktemp "$0XXXX.pid")
+    echo "$1" > $f
+}
+
+
+remove_record() {
+
+    for file in $0*.pid ; do
+	cat $file
+	rm $file
+    done
+}
+
+
+$(ls &>/dev/null) &
+childpid=$!
+record_child_pid $childpid
+sleep 2
+remove_record
