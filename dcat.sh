b4b1c23523dd27d6ab2813cdfdfd7c5bebdc4d37
Mike Day <ncmike@ncultra.org>
2016-06-21 15:52:28 -0400

    added documentation to dcat.sh

--- dcat.sh
+++ dcat.sh
@@ -1,5 +1,23 @@
-
 #!/usr/bin/env bash
+
+# run this program to stress large-file network i/o 
+# The server (listener should) be a host that you want to stress
+# The client will streem the contents of /dev/urandom over TCP
+# to the host. Set $CHILDREN to the number of clients
+#
+# to run a host as a server:
+#
+# dcat.sh 0 (from the host's login shell)
+# 
+# to start 20 clients that will stress the host: 
+#
+# dcat.sh 20 <host ip>
+#
+# To kill the clients:
+#
+# dcat.sh -1 
+
+
 
 kill_children() {
     ps --no-headers -C ncat | awk '{print $1}' | xargs kill &> /dev/null
@@ -9,7 +27,10 @@ kill_children() {
 
 # if $CHILDREN == 0, start a server
 # if $CHILDREN == -1, kill all the tests
+# if $CHILDREN > zero, start that many ncat clients
 CHILDREN=$1
+
+# ip address of the ncat listener. 
 SERVER=$2
 if   (( $CHILDREN == 0 )) ; then 
 
