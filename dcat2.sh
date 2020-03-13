7de718c47b611f7b300e1bf6b79a41966c309d83
Mike Day <michael.d.day.ii@lmco.com>
2020-03-13 15:43:05 -0400

    starting new improved dcat

---
+++ dcat2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# -s start as a server (keep listening)
+# -h host (listener ip address)
+# -p port (listener port)
+# -c <num> start <num> clients
+# -t use tcp
+# -u use udp
+# -k kill clients
+# -f cat file (default is /dev/urandom)
